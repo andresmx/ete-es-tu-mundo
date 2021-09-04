59ea4475611075a05d0f862a5f0152fcc463d75d
andres meza <andrumeza2@gmail.com>
2021-09-03 22:37:55 -0500

    todo

---
+++ built/app.d.ts
@@ -0,0 +1,23 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+import * as MRE from '@microsoft/mixed-reality-extension-sdk';
+/**
+ * The main class of this app. All the logic goes here.
+ */
+export default class HelloWorld {
+    private context;
+    private text;
+    private button;
+    private player;
+    private assets;
+    private buzzerSound;
+    constructor(context: MRE.Context);
+    /**
+     * Once the context is "started", initialize the app.
+     */
+    private started;
+    private startSound;
+}
+//# sourceMappingURL=app.d.ts.map
